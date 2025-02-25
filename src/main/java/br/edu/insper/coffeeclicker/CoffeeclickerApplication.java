8047cd587228692e0daada349c1614c496df4de0
arturacruz <arturacruz@icloud.com>
2025-02-25 16:48:48 -0300

    crazy

---
+++ src/main/java/br/edu/insper/coffeeclicker/CoffeeclickerApplication.java
@@ -0,0 +1,13 @@
+package br.edu.insper.coffeeclicker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeclickerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeclickerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/insper/coffeeclicker/CoffeeclickerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.insper.coffeeclicker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeclickerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
